82b731fcbfcb68703407772e3ab8dc34b8b61e4f
thedev-junyoung <thedev.junyoung@gmail.com>
2024-12-13 15:02:50 +0900

    2일차

--- user/infra/repository/user_repo.py
+++ user/infra/repository/user_repo.py
@@ -1,5 +1,49 @@
+from fastapi import HTTPException
+
+from database import SessionLocal
 from user.domain.repository.user_repo import IUserRepository
+from user.domain.user import User as UserVO
+from user.infra.db_models.user import User
 
 
 class UserRepository(IUserRepository):
-    pass
+    def save(self, user: UserVO):
+        new_user = User(
+            id=user.id,
+            name=user.name,
+            email=user.email,
+            password=user.password,
+            memo=user.memo,
+            created_at=user.created_at,
+            updated_at=user.updated_at,
+        )
+
+        with SessionLocal() as db:
+            db.add(new_user)
+            db.commit()
+
+    def update(self, user_vo: UserVO):
+        with SessionLocal() as db:
+            user = db.query(User).filter(User.id == user_vo.id).first()
+
+            if not user:
+                raise HTTPException(status_code=422)
+
+            user.name = user_vo.name
+            user.password = user_vo.password
+
+            db.add(user)
+            db.commit()
+
+        return user
+
+
+    def delete(self, id: str):
+        with SessionLocal() as db:
+            user = db.query(User).filter(User.id == id).first()
+
+            if not user:
+                raise HTTPException(status_code=422)
+
+            db.delete(user)
+            db.commit()
