00198b9746eadf1d4587be3ab3cb38a61f601980
thedev-junyoung <thedev.junyoung@gmail.com>
2024-12-13 14:51:33 +0900

    2일차

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from starlette.responses import JSONResponse
+
 from user.interface.controller.user_controller import router as user_router
 app = FastAPI()
 
 app.include_router(user_router)
+
+@app.exception_handler(RequestValidationError) # RequestValidationError 예외가 발생하면 처리하는 핸들러
+async def validation_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=400, # 상태 코드를 400으로 설정한다.
+        content=exc.errors(), # 에러 메시지를 반환한다.
+    )
+
 
 @app.get("/")
 def hello():
--- user/interface/controller/user_controller.py
+++ user/interface/controller/user_controller.py
@@ -1,11 +1,26 @@
 
 from fastapi import APIRouter, BackgroundTasks, Depends
+from pydantic.v1 import BaseModel
+
+from user.application.user_service import UserService
 
 router = APIRouter(prefix="/users")
 
+class CreateUserBody(BaseModel): # 파이단틱의 모델을 사용하여 유저 생성 요청을 받는다.
+    name: str
+    email: str
+    password: str
+
 @router.post("", status_code=201)
-def create_user():
-    return "create_user"
+def create_user(user: CreateUserBody): #
+    user_service = UserService()
+    created_user = user_service.create_user(
+        name=user.name,
+        email=user.email,
+        password=user.password,
+    )
+
+    return created_user
 
 
 
